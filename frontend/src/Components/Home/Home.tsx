66939dee4f294f9dd27c167ecd0a1d3dcfa93f8a
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-06-27 16:04:22 +0530

    tailwind and router init

---
+++ frontend/src/Components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Body from './Body'
+
+function Home() {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <Body></Body>
+
+    </div>
+  )
+}
+
+export default Home
